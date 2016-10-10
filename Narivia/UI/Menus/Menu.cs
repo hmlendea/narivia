45e477523ad13fad6a52c9d45d359e6157f0415a
Horatiu Mlendea <Mlendea.Horatiu@GMail.com>
2016-10-10 11:42:25 +0300

    Implemented menus

---
+++ Narivia/UI/Menus/Menu.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Narivia.UI.Screens;
+using Narivia.UI.Input;
+
+namespace Narivia.UI.Menus
+{
+    public class Menu
+    {
+        private string id;
+        
+        public string ID
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnMenuChange(this, null);
+            }
+        }
+
+        public string Axis { get; set; }
+
+        public string Effects { get; set; }
+
+        [XmlElement("Item")]
+        public List<MenuItem> Items { get; set; }
+
+        [XmlIgnore]
+        public int ItemNumber { get; private set; }
+        
+        public event EventHandler OnMenuChange;
+        
+        public Menu()
+        {
+            id = string.Empty;
+            ItemNumber = 0;
+            Axis = "Y";
+            Effects = string.Empty;
+            Items = new List<MenuItem>();
+        }
+        
+        public void LoadContent()
+        {
+            string[] split = Effects.Split(':');
+
+            foreach (MenuItem item in Items)
+            {
+                item.Image.LoadContent();
+
+                foreach (string s in split)
+                    item.Image.ActivateEffect(s);
+            }
+
+            AlignMenuItems();
+        }
+
+        public void UnloadContent()
+        {
+            foreach (MenuItem item in Items)
+                item.Image.UnloadContent();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if ("Xx".Contains(Axis))
+            {
+                if (InputManager.Instance.KeyPressed(Keys.Right))
+                    ItemNumber++;
+                else if (InputManager.Instance.KeyPressed(Keys.Left))
+                    ItemNumber--;
+            }
+            else if ("Yy".Contains(Axis))
+            {
+                if (InputManager.Instance.KeyPressed(Keys.Down))
+                    ItemNumber++;
+                else if (InputManager.Instance.KeyPressed(Keys.Up))
+                    ItemNumber--;
+            }
+
+            if (ItemNumber < 0)
+                ItemNumber = 0;
+            else if (ItemNumber > Items.Count - 1)
+                ItemNumber = Items.Count - 1;
+
+            for (int i = 0; i < Items.Count; i++)
+            {
+                if (i == ItemNumber)
+                    Items[i].Image.Active = true;
+                else
+                    Items[i].Image.Active = false;
+
+                Items[i].Image.Update(gameTime);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (MenuItem item in Items)
+                item.Image.Draw(spriteBatch);
+        }
+
+        public void Transition(float alpha)
+        {
+            foreach (MenuItem item in Items)
+            {
+                item.Image.Active = true;
+                item.Image.Opacity = alpha;
+
+                if (alpha == 0.0f)
+                    item.Image.FadeEffect.Increasing = true;
+                else
+                    item.Image.FadeEffect.Increasing = false;
+            }
+        }
+
+        void AlignMenuItems()
+        {
+            Vector2 dimensions = Vector2.Zero;
+
+            foreach (MenuItem item in Items)
+                dimensions += new Vector2(item.Image.SourceRectangle.Width, item.Image.SourceRectangle.Height);
+
+            dimensions = new Vector2(
+                (ScreenManager.Instance.Dimensions.X - dimensions.X) / 2,
+                (ScreenManager.Instance.Dimensions.Y - dimensions.Y) / 2);
+
+            foreach (MenuItem item in Items)
+            {
+                if (Axis == "X" || Axis == "x")
+                    item.Image.Position = new Vector2(
+                        dimensions.X,
+                        (ScreenManager.Instance.Dimensions.Y - item.Image.SourceRectangle.Height) / 2);
+                else if (Axis == "Y" || Axis == "y")
+                    item.Image.Position = new Vector2(
+                        (ScreenManager.Instance.Dimensions.X - item.Image.SourceRectangle.Width) / 2,
+                        dimensions.Y);
+
+                dimensions += new Vector2(item.Image.SourceRectangle.Width, item.Image.SourceRectangle.Height);
+            }
+        }
+    }
+}
+
---
+++ Narivia/UI/Menus/MenuManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Narivia.UI.Screens;
+using Narivia.UI.Input;
+
+
+namespace Narivia.UI.Menus
+{
+    public class MenuManager
+    {
+        /*===== Variables =====*/
+
+        Menu menu;
+        bool transitioning;
+
+        /*===== Constructor =====*/
+
+        public MenuManager()
+        {
+            menu = new Menu();
+            menu.OnMenuChange += menu_OnMenuChange;
+            transitioning = false;
+        }
+
+        public void LoadContent(string menuPath)
+        {
+            if (menuPath != string.Empty)
+                menu.ID = menuPath;
+        }
+
+        public void UnloadContent()
+        {
+            menu.UnloadContent();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (!transitioning)
+                menu.Update(gameTime);
+
+            if (InputManager.Instance.KeyPressed(Keys.Enter) && !transitioning)
+            {
+                MenuItem selectedMenuItem = menu.Items[menu.ItemNumber];
+
+                if (selectedMenuItem.LinkType == "Screen")
+                    ScreenManager.Instance.ChangeScreens(selectedMenuItem.LinkId);
+                else if (selectedMenuItem.LinkType == "Menu")
+                {
+                    transitioning = true;
+                    menu.Transition(1.0f);
+
+                    foreach (MenuItem item in menu.Items)
+                        item.Image.ActivateEffect("FadeEffect");
+                }
+            }
+
+            Transition(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            menu.Draw(spriteBatch);
+        }
+
+        void Transition(GameTime gameTime)
+        {
+            if (transitioning)
+            {
+                int oldMenuCount = menu.Items.Count;
+
+                for (int i = 0; i < oldMenuCount; i++)
+                {
+                    menu.Items[i].Image.Update(gameTime);
+
+                    float first = menu.Items[0].Image.Opacity;
+                    float last = menu.Items[menu.Items.Count - 1].Image.Opacity;
+
+                    if (first == 0.0f && last == 0.0f)
+                        menu.ID = menu.Items[menu.ItemNumber].LinkId;
+                    else if (first == 1.0f && last == 1.0f)
+                        transitioning = false;
+                }
+            }
+        }
+
+        void menu_OnMenuChange(object sender, EventArgs e)
+        {
+            if (menu.ID == null)
+                return;
+
+            XmlScreenManager<Menu> xmlMenuManager = new XmlScreenManager<Menu>();
+
+            menu.UnloadContent();
+            menu = xmlMenuManager.Load(menu.ID);
+            menu.LoadContent();
+
+            menu.OnMenuChange += menu_OnMenuChange;
+            menu.Transition(0.0f);
+
+            foreach (MenuItem item in menu.Items)
+                item.Image.ActivateEffect("FadeEffect");
+        }
+    }
+}
+
