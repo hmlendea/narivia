7564cbd962c26b716a2a7d00ceaa79814e6b5aaa
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-05-25 23:08:19 +0300

    Better blitzkrieg testing

--- Narivia.BlitzkriegTester/Program.cs
+++ Narivia.BlitzkriegTester/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 using Narivia.BusinessLogic.GameManagers;
 
@@ -9,18 +11,54 @@ namespace Narivia.BlitzkriegTester
         const string WORLD_ID = "narivia";
         const string FACTION_ID = "alpalet";
         const string FACTION_TARGET_ID = "caravenna";
+        const int TEST_COUNT = 10;
 
         public static void Main(string[] args)
         {
+            // Preload that area on the storage device
+            GameDomainService game = new GameDomainService();
+            game.NewGame(WORLD_ID, FACTION_ID);
+
             TestSequencialBlitzkrieg();
             TestParallelizedBlitzkrieg();
         }
 
-        public static void TestSequencialBlitzkrieg()
+        static void TestSequencialBlitzkrieg()
         {
             Console.WriteLine("==== RUNNING THE SEQUENCIAL TEST");
-            DateTime startDate = DateTime.Now;
+            List<Tuple<DateTime, DateTime>> results = new List<Tuple<DateTime, DateTime>>();
 
+            for (int i = 0; i < TEST_COUNT; i++)
+            {
+                DateTime startDate = DateTime.Now;
+                CallSequencialBlitzkrieg();
+                DateTime endDate = DateTime.Now;
+
+                results.Add(new Tuple<DateTime, DateTime>(startDate, endDate));
+            }
+
+            DisplayResults(results);
+        }
+
+        static void TestParallelizedBlitzkrieg()
+        {
+            Console.WriteLine("==== RUNNING THE PARALLEL TEST");
+            List<Tuple<DateTime, DateTime>> results = new List<Tuple<DateTime, DateTime>>();
+
+            for (int i = 0; i < TEST_COUNT; i++)
+            {
+                DateTime startDate = DateTime.Now;
+                CallParallelizedBlitzkrieg();
+                DateTime endDate = DateTime.Now;
+
+                results.Add(new Tuple<DateTime, DateTime>(startDate, endDate));
+            }
+
+            DisplayResults(results);
+        }
+
+        static void CallSequencialBlitzkrieg()
+        {
             GameDomainService game = new GameDomainService();
             game.NewGame(WORLD_ID, FACTION_ID);
 
@@ -29,19 +67,13 @@ namespace Narivia.BlitzkriegTester
             while (regionId != null)
             {
                 regionId = game.Blitzkrieg_Seq(FACTION_ID, FACTION_TARGET_ID);
-
-                Console.WriteLine(regionId);
             }
 
-            DateTime endDate = DateTime.Now;
-            DisplayResults(startDate, endDate);
+            game = null;
         }
 
-        public static void TestParallelizedBlitzkrieg()
+        static void CallParallelizedBlitzkrieg()
         {
-            Console.WriteLine("==== RUNNING THE PARALLEL TEST");
-            DateTime startDate = DateTime.Now;
-
             GameDomainService game = new GameDomainService();
             game.NewGame(WORLD_ID, FACTION_ID);
 
@@ -50,22 +82,21 @@ namespace Narivia.BlitzkriegTester
             while (regionId != null)
             {
                 regionId = game.Blitzkrieg_Parallel(FACTION_ID, FACTION_TARGET_ID);
-
-                Console.WriteLine(regionId);
             }
-
-            DateTime endDate = DateTime.Now;
-            DisplayResults(startDate, endDate);
         }
 
-        static void DisplayResults(DateTime startDate, DateTime endDate)
+        static void DisplayResults(List<Tuple<DateTime, DateTime>> results)
         {
-            TimeSpan deltaTime = endDate - startDate;
+            Console.WriteLine($"Results: ({results.Count})");
 
-            Console.WriteLine("Results:");
-            Console.WriteLine($"Started at   : {startDate} system time");
-            Console.WriteLine($"Ended at     : {endDate} system time");
-            Console.WriteLine($"Elpased time : {deltaTime.Milliseconds} ms");
+            List<TimeSpan> durations = results.Select(x => x.Item2 - x.Item1).ToList();
+
+            for (int i = 0; i < TEST_COUNT; i++)
+            {
+                Console.WriteLine($"  {i + 1} : {durations[i].Milliseconds} ms");
+            }
+
+            Console.WriteLine($"Average timespan: {Math.Round(durations.Average(x => x.Milliseconds))} ms");
         }
     }
 }
