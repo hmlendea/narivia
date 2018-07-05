ca5e09fe6d595c897a156573f41f45881341b926
Horatiu Mlendea <mlendea.horatiu@gmail.com>
2018-07-05 18:11:15 +0300

    Values for the TileShape enum entries

--- Narivia.Gui/Helpers/TileShape.cs
+++ Narivia.Gui/Helpers/TileShape.cs
@@ -2,23 +2,23 @@
 {
     public enum TileShape
     {
-        None,
-        Middle,
-        LonePatch,
+        Middle = 10,
+        SingleBig = 0,
+        SingleSmall = 3,
 
-        EdgeN,
-        EdgeW,
-        EdgeS,
-        EdgeE,
+        EdgeN = 7,
+        EdgeW = 9,
+        EdgeS = 13,
+        EdgeE = 11,
 
-        InnerCornerNW,
-        InnerCornerNE,
-        InnerCornerSW,
-        InnerCornerSE,
+        InnerCornerNW = 1,
+        InnerCornerNE = 2,
+        InnerCornerSW = 4,
+        InnerCornerSE = 5,
 
-        OuterCornerNW,
-        OuterCornerNE,
-        OuterCornerSW,
-        OuterCornerSE
+        OuterCornerNW = 6,
+        OuterCornerNE = 8,
+        OuterCornerSW = 12,
+        OuterCornerSE = 14
     }
 }
