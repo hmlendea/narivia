96a95b8e58e19489fd1f9d34292684978841c203
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-02-26 00:56:42 +0200

    Build and install FreeImage from source

--- .travis/monogame-install.sh
+++ .travis/monogame-install.sh
@@ -3,15 +3,24 @@
 
 MONOGAME_VERSION="3.6"
 INSTALLER_EXE="monogame-sdk.run"
-DOWNLOAD_URL="http://www.monogame.net/releases/v$MONOGAME_VERSION/$INSTALLER_EXE"
+FREEIMAGE_ZIP="FreeImage3170.zip"
+FREEIMAGE_DOWNLOAD_URL="http://downloads.sourceforge.net/freeimage/$FREEIMAGE_ZIP"
+MONOGAME_DOWNLOAD_URL="http://www.monogame.net/releases/v$MONOGAME_VERSION/$INSTALLER_EXE"
 MONOGAME_DIR=$(pwd)"/monogame"
 POSTINSTALL_SCRIPT="postinstall.sh"
 ORIGINAL_DIR=$(pwd)
 
-echo " >>> Installing gtk-sharp3"
+echo " >>> Installing GTK#3"
 sudo apt-get install gtk-sharp3
 
-echo " >>> Installing libfreeimage3"
+echo " >>> Installing FreeImage 3.17"
+wget -c "$FREEIMAGE_DOWNLOAD_URL"
+unzip "$FREEIMAGE_ZIP"
+cd "FreeImage"
+make
+sudo make install
+cd "$ORIGINAL_DIR"
+
 sudo apt-get install libfreeimage3
 sudo ln -s "/usr/lib/x86_64/libfreeimage-3.17.0.so" "/usr/lib/libfreeimage-3.17.0.so"
 
@@ -19,22 +28,17 @@ echo " >>> Installing ttf-mscorefonts-installer"
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt-get install ttf-mscorefonts-installer
 
-echo " >>> Downloading the MonoGame SDK v$MONOGAME_VERSION Installer"
-wget -c "$DOWNLOAD_URL"
-
-echo " >>> Running the MonoGame installer"
+echo " >>> Installing the MonoGame SDK v$MONOGAME_VERSION Installer"
+wget -c "$MONOGAME_DOWNLOAD_URL"
 chmod +x monogame-sdk.run
 sudo "./$INSTALLER_EXE" --noexec --keep --target "$MONOGAME_DIR"
 
-echo " >>> Entering the '$MONOGAME_DIR' directory"
-cd "$MONOGAME_DIR"
-
 echo " >>> Removing the user input prompt from the post-installation script"
+cd "$MONOGAME_DIR"
 sudo chmod 777 "$POSTINSTALL_SCRIPT"
 sudo sed -i '62,66d' "$POSTINSTALL_SCRIPT"
 
+echo " >>> Running the MonoGame post-installation script"
 sudo chmod +x "$POSTINSTALL_SCRIPT"
 sudo "./$POSTINSTALL_SCRIPT"
-
-echo " >>> Going back into the '$ORIGINAL_DIR' directory"
 cd "$ORIGINAL_DIR"
