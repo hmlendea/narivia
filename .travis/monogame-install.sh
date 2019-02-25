4877cb13e2014ca7a48714147e6b35258382321d
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-02-25 20:43:14 +0200

    Better MG installation script

--- .travis/monogame-install.sh
+++ .travis/monogame-install.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-# MonoGame 3.6 SDK Installation Script for Travis-CI Virtual Machines
+# MonoGame SDK Installation Script for Travis-CI Virtual Machines
 
 MONOGAME_VERSION="3.6"
 INSTALLER_EXE="monogame-sdk.run"
 DOWNLOAD_URL="http://www.monogame.net/releases/v$MONOGAME_VERSION/$INSTALLER_EXE"
 MONOGAME_DIR="./monogame"
-POSTINSTALL_SCRIPT="$MONOGAME_DIR/postinstall.sh"
+POSTINSTALL_SCRIPT="postinstall.sh"
+ORIGINAL_DIR=$(pwd)
 
 echo " >>> Installing gtk-sharp3"
 sudo apt-get install gtk-sharp3
 
 echo " >>> Downloading the MonoGame SDK v$MONOGAME_VERSION Installer"
-wget $DOWNLOAD_URL
+wget "$DOWNLOAD_URL"
 
+echo " >>> Running the MonoGame installer"
 chmod +x monogame-sdk.run
 sudo "./$INSTALLER_EXE" --noexec --keep --target "$MONOGAME_DIR"
-sudo chmod 777 ./monogame/postinstall.sh
+
+echo " >>> Entering the '$MONOGAME_DIR' directory"
+cd "$MONOGAME_DIR"
 
 echo " >>> Removing the user input prompt from the post-installation script"
+sudo chmod 777 "$POSTINSTALL_SCRIPT"
 sudo sed -i '62,66d' "$POSTINSTALL_SCRIPT"
 
 sudo chmod +x "$POSTINSTALL_SCRIPT"
 sudo "$POSTINSTALL_SCRIPT"
+
+echo " >>> Going back into the '$ORIGINAL_DIR' directory"
+cd "$ORIGINAL_DIR"
